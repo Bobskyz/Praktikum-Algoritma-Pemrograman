cfd551adbc5b04e9dce1c97493f905556d29c857
Dafin Surya <145106018+Bobskyz@users.noreply.github.com>
2023-11-17 14:51:59 +0700

    Add files via upload

---
+++ Job Sheet 2/js2_lth1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+	char nama[50];
+	
+	printf("Hello, siapa nama lengkapmu?\n= ");
+	scanf("%s", &nama);
+	printf("\nSelamat datang %s dalam pemrograman C!\n", nama);
+	
+	return 0;
+}
