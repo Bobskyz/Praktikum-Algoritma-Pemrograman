e4705d8e19a65c222e4268ee5a4271cb0432a5a1
Dafin Surya <145106018+Bobskyz@users.noreply.github.com>
2023-11-20 19:25:24 +0700

    Add files via upload

---
+++ Job Sheet 07/Source Code Latihan 1.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main (){
+	int ditemukan, cari, NilaiRtRt[20] = {59, 67, 92, 83, 74, 91, 78, 94, 88, 84,
+										  80, 70, 82, 76, 79, 58, 52, 75, 63, 69};
+					   
+	printf ("Inputkan Nilai rata-rata yang akan dicari (0-100): ");
+	scanf ("%d", &cari);
+	
+	for (int a = 0; a < 20; a++){
+		if (cari == NilaiRtRt[a]){
+			printf ("\nNilai Rata-Rata tersebut berada pada data Mahasiswa ke-%d\n", a+1);
+			ditemukan = 1;
+			break;
+		}
+	}
+	
+	if (ditemukan != 1){
+		printf ("\nNilai Rata-Rata tersebut tidak ditemukan\n");
+	}
+	
+	return 0;
+}
