7ec7ed96978c2c649ad024d838553beb692f0148
Dafin Surya <145106018+Bobskyz@users.noreply.github.com>
2023-11-29 15:26:17 +0700

    Update Source Code Latihan 1.cpp

--- Job Sheet 07/Source Code Latihan 1.cpp
+++ Job Sheet 07/Source Code Latihan 1.cpp
@@ -1,8 +1,8 @@
+//Developed by : Bobskyz
 #include<stdio.h>
 
 int main (){
-	int ditemukan, cari, NilaiRtRt[20] = {59, 67, 92, 83, 74, 91, 78, 94, 88, 84,
-										  80, 70, 82, 76, 79, 58, 52, 75, 63, 69};
+	int ditemukan, cari, NilaiRtRt[20] = {59, 67, 92, 83, 74, 91, 78, 94, 88, 84, 80, 70, 82, 76, 79, 58, 52, 75, 63, 69};
 					   
 	printf ("Inputkan Nilai rata-rata yang akan dicari (0-100): ");
 	scanf ("%d", &cari);
