7665182ac42e78dfc765f910e1f365cae6649d58
Dafin Surya <145106018+Bobskyz@users.noreply.github.com>
2023-12-04 20:28:47 +0700

    Update SC Latihan 2.cpp

--- JobSheet7-Array/Source Code/SC Latihan 2.cpp
+++ JobSheet7-Array/Source Code/SC Latihan 2.cpp
@@ -1,26 +1,21 @@
 //Developed by : Dafin Surya
 #include<stdio.h>
-#include<stdlib.h>
 
 int main(){
 	int banyak;
 	
 	printf ("Inputkan banyak siswa : ");
-	scanf ("%d", &banyak);
+	scanf ("%d", &banyak); fflush (stdin);
 	printf ("\n");
 	
-	char NamaMhs[banyak][20];
+	char NamaMhs[banyak][50];
 	
 	for (int a = 0; a < banyak; a++){
 		printf ("Inputkan Nama Mahasiswa ke-%d : ", a+1);
-		scanf ("%s", &NamaMhs[a]);
+		gets(NamaMhs[a]);
 	}
-	
-	system("cls");
-	
 	for (int b = 0; b < banyak; b++){
-		printf ("Nama Mahasiswa ke-%d : %s\n", b+1, NamaMhs[b]);
+		printf ("\nNama Mahasiswa ke-%d : %s", b+1, NamaMhs[b]);
 	}
-	
 	return 0;
 }
